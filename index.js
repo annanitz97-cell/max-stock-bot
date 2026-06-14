845dff26aef4f220ae68fd2f6c2c4b5dd26a2c63
annanitz97-cell <annanitz97@gmail.com>
2026-06-14 18:22:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("MAX stock bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
