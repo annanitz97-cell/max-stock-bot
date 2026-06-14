8a54e9b49d8ff21c4be422729991d702f2ec6572
annanitz97-cell <annanitz97@gmail.com>
2026-06-14 18:46:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("MAX stock bot is running");
+const SHEET_ID = process.env.SHEET_ID;
+
+async function loadData() {
+const url = https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv;
+
+const response = await fetch(url);
+const text = await response.text();
+
+return text;
+}
+
+app.get("/", async (req, res) => {
+try {
+const data = await loadData();
+
+res.send(data.substring(0, 3000));
+
+} catch (e) {
+res.send(e.message);
+}
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
+console.log(Server started on ${PORT});
 });
